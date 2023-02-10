09012e5a3461a40075fa861d8b4368c6a8ed90da
Jose Reyes <jo.reyesgon@gmail.com>
2023-02-10 20:00:23 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Prueba from './js/components/Prueba';
+
+function App() {
+  return (
+    <div className="App">
+      <Prueba />
+    </div>
+  );
+}
+
+export default App;
